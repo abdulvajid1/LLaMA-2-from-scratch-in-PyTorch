45df0fde6e12829e3e600aab5dbcc3ff362bdd4e
Abdul Vajid CA <vajidca444@gmail.com>
2025-03-27 07:49:36 +0530

    added top_p method

--- inferance.py
+++ inferance.py
@@ -52,12 +52,98 @@ class Llama:
         
         return Llama(model, tokenizer, modelargs)
     
+    def text_completion(self, prompts:list[str], temperature: float=0.6,top_p: float=0.9, max_gen_len:Optional[int]=None):
+        if max_gen_len is None:
+            max_gen_len = self.model_args.max_seq_len - 1
+
+        # Tokenize the multiple prompts at once 
+        prompts_tokens =[self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=True) for prompt in prompts]
+
+        # make sure batch_size in not over size
+        batch_size = len(prompts_tokens) # consider the number of prompts at once as the batch size
+        assert batch_size <= self.model_args.max_batch_size
+
+        # make sure the prompt size is not over the limit
+        max_prompt_len = max((len(prompt)) for prompt in prompts_tokens)
+        assert max_prompt_len <= self.model_args.max_seq_len
+
+        total_len = min(self.model_args.max_seq_len, max_prompt_len+max_gen_len )
+
+        # Initialize a Tensor with max size of the output token with batchsize full of pad_token
+        pad_id = self.tokenizer.pad_id()
+        token = torch.full((batch_size, total_len),
+                           fill_value=pad_id,
+                            dtype=torch.long,
+                            device=device)
+        
+        # Initialize at the start the prompt token for each batch tokens
+        for k, t in enumerate(prompts_tokens):
+            token[k, :len(t)] = torch.tensor(t, dtype=torch.long, device=device)
+
+        # Initialize the end of token  for each batch to identify any of the batch get end token init
+        eos_reached = torch.Tensor([False]*batch_size, device=device)
+
+        prompt_token_mask = token != pad_id
+
+        for cur_pos in tqdm(range(1, total_len), desc='Genarating next token'):
+            with torch.no_grad():
+                logits = self.model.forward(token[:, cur_pos-1: cur_pos],cur_pos)
+
+            if temperature > 0:
+                # The temperature is applied Before the softmax
+                probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
+                next_token = self._sample_top_p(probs, top_p)
+            else:
+                # Greedy token
+                next_token = torch.argmax(logits, dim=-1, keepdim=True)
+
+    
+            next_token = next_token.reshape(-1)
+            next_token = torch.where(prompt_token_mask[:, cur_pos], token[:, cur_pos], next_token)
+            token[:, cur_pos] = next_token
+
+            # check if any of batch's token is not a prompt token which then will be a pad token and the next token is eos token
+            # then for the current batch we update the eos_reached token as True
+            eos_reached = eos_reached | (~prompt_token_mask[:, cur_pos]) & (next_token==self.tokenizer.eos_id())
+            
+            # check if all prompts (batch) got eos_token, stop genaration
+            if all(eos_reached):
+                break
+
+            out_token = []
+            out_text = []
+
+            # itrate through each prompts output
+            for prompt_token, curr_prompt_tokens in enumerate(token.tolist()):
+                if self.tokenizer.eos_id() in curr_prompt_tokens:
+                    eos_index = curr_prompt_tokens.index(self.tokenizer.eos_id())
+                    curr_prompt_tokens = curr_prompt_tokens[:eos_index]
+                out_token.append(curr_prompt_tokens)
+                out_text.append(self.tokenizer.decode(curr_prompt_tokens))
+            return (out_text, out_token)
+        
+        def _sample_top_p(self, probs, p):
+            probs, prob_idx = torch.sort(probs, dim=-1, descending=True)
+            cumsum_prob = torch.cumsum(probs,dim=-1)
+            p_mask = cumsum_prob - probs > p
+            probs[p_mask] = 0 
+            probs = probs.div(probs.sum(dim=-1,keepdim=True))
+            next_token = torch.multinomial(probs, num_samples=1)
+            next_token = torch.gather(prob_idx, -1, next_token)
+
+            return next_token
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     torch.manual_seed(0)
     allow_cuda = False
-    device = 'cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'\
-    
+    device = ('cuda' if torch.cuda.is_available() and allow_cuda else 'cpu')
     model = Llama.build(
         checkpoint_dir='llama-2-7b/',
         tokenizer_path='tokenizer.model',
@@ -71,6 +157,5 @@ if __name__ == '__main__':
 
 
     # inferance
-
 
 
